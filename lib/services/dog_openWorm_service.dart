dcc282142a4a6071e1a4e4f3aae3d7ade1f00cd5
Sergio-Cordeiro <62566660+Sergio-Cordeiro@users.noreply.github.com>
2023-04-09 03:16:13 -0300

    new vaccines

--- lib/services/dog_openWorm_service.dart
+++ lib/services/dog_openWorm_service.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:canine_shield/data/vaccines_data.dart';
 
 class DogOpenWormService {
   static const String _baseUrl = 'https://dog-api.kinduff.com/api/v1/vaccines';
 
   static Future<List<String>> getDogVaccines(String breed) async {
-    final response = await http.get(Uri.parse('$_baseUrl/$breed'));
+    final response = await http.get(Uri.parse(_baseUrl));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       return List<String>.from(data['vaccines']);
     } else {
-      throw Exception('Failed to load dog vaccines');
+      return dogVaccines;
     }
   }
 }
